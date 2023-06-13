df0b0885885ff49d38aac5b512b3e599d0fdbfb9
woohyeon <wh8187@gmail.com>
2023-06-14 01:53:42 +0900

    개인페이지 웹 CSS 수정 및 모달 CSS,JS 수정

--- mypage/js/sidebar_menu.js
+++ mypage/js/sidebar_menu.js
@@ -1,43 +1,44 @@
 $(document).ready(function () {
-  // $('.toggle').on('click', function () {
-  //   $('.toggle, .overlay, .gnb').toggleClass('active');
-  //   // $('.overlay').toggleClass('active');
-  //   // $('.gnb').toggleClass('active');
-  // });
-  // $('.gnb a').on('click',function(){
-  //   $('.toggle, .overlay, .gnb').removeClass('active');
-  // });
   $(".sidebar_menu li a").click(function () {
     $(".sidebar").toggle();
     $("#trigger").prop("checked", $(".sidebar").is(":visible"));
   });
-  $('#trigger').change(function() {
-    if ($(this).is(':checked')) {
-      $('.sidebar').show();
+  $("#trigger").change(function () {
+    if ($(this).is(":checked")) {
+      $(".sidebar").show();
     } else {
-      $('.sidebar').hide();
+      $(".sidebar").hide();
     }
   });
-  $('.top_button').click(function(e) {
-    e.preventDefault();
-    var target = $(this).attr('href');
-    var offset = $(target).offset().top;
-
-    $('html, body').animate({ scrollTop: offset }, 'slow', function() {
-      window.location.hash = target;
-    });
+  // sec_4_modal
+  $(".sec_4_de_photo").on("click", function(e) {
+    e.stopPropagation(); // 이벤트 버블링 막기
+    $(this).siblings(".modal").fadeIn();
   });
-  $('.sec_4_de_photo').click(function() {
-    $(this).siblings().fadeIn();
+  $(".close-btn").on("click", function () {
+    $(".modal").fadeOut();
   });
-
-  $('.close-modal').click(function() {
-    $('.modal_design').fadeOut();
-  });
-
+  // modal 밖의 요소를 클릭했을때, modal창 없애기
   $(document).on('click', function(event) {
-    if ($(event.target).hasClass('modal')) {
+    var modal = $('.modal_container');
+    if (!modal.is(event.target) && modal.has(event.target).length === 0) {
       $('.modal').fadeOut();
     }
+  });
+  
+  //modal이 fadein 됐을때 ESC키를 눌러 modal창 없애기
+  $(document).on('keydown', function(event) {
+    if (event.key === 'Escape') {
+      $('.modal').fadeOut();
+    }
+  });
+  //footer:top_button
+  $(".top_button").click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    var offset = $(target).offset().top;
+    $("html, body").animate({ scrollTop: offset }, "slow", function () {
+      window.location.hash = target;
+    });
   });
 });
