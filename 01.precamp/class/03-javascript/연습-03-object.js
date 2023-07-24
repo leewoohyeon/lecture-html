e6094fe6b92b6f3a9e8b108429b037adade119f4
woohyeon <wh8187@gmail.com>
2023-07-24 22:41:46 +0900

    [인강]인프런-싸이월드 만들기 1탄, js 변수, 상수, 객체, 배열 공부하기

---
+++ 01.precamp/class/03-javascript/연습-03-object.js
@@ -0,0 +1,14 @@
+let friend = {
+  name : "우현",
+  age : 23,
+  camp : "코드캠프"
+}
+undefined
+friend
+// {name: '우현', age: 23, camp: '코드캠프'}
+friend.name
+// '우현'
+friend.age
+// 23
+friend.camp
+// '코드캠프'
